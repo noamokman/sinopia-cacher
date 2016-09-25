04261eae249ae28b28b5fc17c35cab4ba45fd624
Noam Okman <noamokman@gmail.com>
2016-09-25 12:24:55 +0300

    maintenance

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var npm = require('npm');
-var mkdirp = require('mkdirp');
-var rimraf = require('rimraf');
-var sinopia = require('sinopia');
-var path = require('path');
-var Q = require('q');
-var _ = require('lodash');
-var fs = require('fs');
-var inquirer = require("inquirer");
-var ncp = require('ncp').ncp;
+const npm = require('npm');
+const sinopia = require('sinopia');
+const path = require('path');
+const pify = require('pify');
+const rimraf = pify(require('rimraf'));
+const mkdirp = pify(require('mkdirp'));
+const _ = require('lodash');
+const fs = pify(require('fs'));
+const inquirer = require("inquirer");
+const ncp = require('ncp').ncp;
 
 ncp.limit = 16;
 
 var server;
 
-var paths = {
+const paths = {
   temp: '.tmp',
   storage: '.tmp/storage',
   export: 'export/storage'
@@ -23,23 +23,21 @@ var packagesToInstall = [];
 var packagesAlreadyInstalled = [];
 
 function getFolderPackages() {
-  return Q.nfcall(fs.readdir, paths.storage)
-    .then(function (files) {
-      return _.without(files, '.sinopia-db.json');
-    });
+  return fs.readdir(paths.storage)
+    .then(files => _.without(files, '.sinopia-db.json'));
 }
 
 function reinstallReq() {
   if (packagesToInstall.length == 0) {
-    return Q();
+    return Promise.resolve();
   }
 
-  return Q.ninvoke(npm.commands, 'install', paths.temp, packagesToInstall)
+  return pify(npm.commands.install.bind(npm.commands))(paths.temp, packagesToInstall)
     .then(function () {
       packagesAlreadyInstalled = packagesAlreadyInstalled.concat(packagesToInstall);
 
       return getFolderPackages();
-    }, function(err) {console.log(err);})
+    }, err => console.log(err))
     .then(function (folderPackages) {
       packagesToInstall = _.difference(folderPackages, packagesAlreadyInstalled);
 
@@ -48,16 +46,10 @@ function reinstallReq() {
 }
 
 function megaFunction() {
-  return Q.nfcall(rimraf, path.resolve(process.env.APPDATA, "npm-cache"))
-    .then(function () {
-      return Q.nfcall(rimraf, paths.temp);
-    })
-    .then(function () {
-      return Q.nfcall(mkdirp, paths.temp);
-    })
-    .then(function () {
-      var deferred = Q.defer();
-
+  return rimraf(path.resolve(process.env.APPDATA, "npm-cache"))
+    .then(() => rimraf(paths.temp))
+    .then(() => mkdirp(paths.storage))
+    .then(() => new Promise((resolve, reject) => {
       server = sinopia({
         storage: paths.storage,
         "uplinks": {
@@ -71,55 +63,44 @@ function megaFunction() {
             "allow_publish": "$all",
             "proxy": "npmjs"
           }
-        }
+        },
+        self_path: paths.temp
       })
-        .listen(4873, 'localhost', deferred.makeNodeResolver());
+        .listen(4873, 'localhost', err=> {
+          if (err) {
+            return reject(err);
+          }
 
-      return deferred.promise;
-    })
-    .then(function () {
-      return Q.ninvoke(npm, 'load', {registry: "http://localhost:4873/"});
-    })
-    .then(function () {
-      npm.on("log", function (message) {
-        console.log(message);
-      });
+          resolve();
+        });
+    }))
+    .then(() => pify(npm.load.bind(npm))({registry: "http://localhost:4873/"}))
+    .then(() => {
+      npm.on("log", message => console.log(message));
     })
     .then(reinstallReq)
-    .then(function () {
+    .then(() => {
       if (server) {
         server.close();
         server = null;
       }
     })
+    .then(() => mkdirp(paths.export))
+    .then(() => pify(ncp)(paths.storage, paths.export))
+    .then(() => rimraf(paths.temp))
+    .then(() => console.log('all done!'))
+    .catch(err => console.error(err))
     .then(function () {
-      return Q.nfcall(mkdirp, paths.export);
-    })
-    .then(function () {
-      return Q.nfcall(ncp, paths.storage , paths.export);
-    })
-    .then(function () {
-      return Q.nfcall(rimraf, paths.temp);
-    })
-    .then(function () {
-      console.log('all done!');
-    })
-    .catch(function (err) {
-      console.error(err);
-    })
-    .finally(function () {
       if (server) {
         server.close();
       }
-	  
-	  process.exit();
+
+      process.exit();
     });
 }
 
 function askPackage() {
-  var deferred = Q.defer();
-
-  inquirer.prompt([
+  return inquirer.prompt([
     {
       type: "input",
       name: "package",
@@ -131,46 +112,37 @@ function askPackage() {
       message: "Want to enter another package (just hit enter for YES)?",
       default: true
     }
-  ], function (answers) {
-    packagesToInstall.push(answers.package);
+  ])
+    .then(answers => {
+      packagesToInstall.push(answers.package);
 
-    if (answers.askAgain) {
-      deferred.resolve(askPackage());
-    } else {
-      deferred.resolve();
-    }
-  });
-
-  return deferred.promise;
+      if (answers.askAgain) {
+        return askPackage();
+      }
+    });
 }
 
-if(process.argv.length > 2)
-{
-	packagesToInstall = process.argv.slice(2);
-	console.log('Going to cache these packages: ' + packagesToInstall.join(', '));
-	
-	megaFunction();
-	return;
+if (process.argv.length > 2) {
+  packagesToInstall = process.argv.slice(2);
+  console.log('Going to cache these packages: ' + packagesToInstall.join(', '));
+
+  megaFunction();
+  return;
 }
 
 askPackage()
   .then(function () {
-    var deferred = Q.defer();
-
     console.log('Going to cache these packages: ' + packagesToInstall.join(', '));
 
-    inquirer.prompt([{
+    return inquirer.prompt([{
       type: "confirm",
       name: "confirm",
       message: "Is this OK (just hit enter for YES)?",
       default: true
-    }], function (answers) {
-      if (answers.confirm) {
-        deferred.resolve(megaFunction());
-      } else {
-        deferred.resolve();
-      }
-    });
-
-    return deferred.promise;
+    }])
+      .then(answers => {
+        if (answers.confirm) {
+          return megaFunction();
+        }
+      });
   });
